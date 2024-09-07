ae2e1e2b3719afc95a94c14cb8d02e5e844c7b73
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2024-09-07 13:46:56 +0800

    feat(update): 2024-09-07
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- foss/dot-var/app/org.gnu.emacs/config/emacs/early-init.el
+++ foss/dot-var/app/org.gnu.emacs/config/emacs/early-init.el
@@ -1,1 +1,18 @@
+;; -*- coding: utf-8 -*-
+;; SPDX-License-Identifier: AGPL-3.0-only
+;; SPDX-FileCopyrightText: Copyright (C) 2024 Rifa Ilyasa Achrinza
+;; SPDX-FileNotice: <text>
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, version 3 of the License.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+;;
+;; You should have received a copy of the GNU Affero General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; </text>
+
 (setq package-enable-at-startup nil)
