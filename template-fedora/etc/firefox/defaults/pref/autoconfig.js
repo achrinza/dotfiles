d515028bcf0030243bab3013ba1983b037da2556
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2024-10-26 23:04:45 +0800

    feat(update): 2024-10-26
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

---
+++ template-fedora/etc/firefox/defaults/pref/autoconfig.js
@@ -0,0 +1,7 @@
+pref("browser.search.suggest.enabled", false);
+pref("browser.urlbar.suggest.bookmark", false);
+pref("browser.urlbar.suggest.engines", false);
+pref("browser.urlbar.suggest.history", false);
+pref("browser.urlbar.suggest.searches", false);
+pref("browser.urlbar.suggest.topsites", false);
+pref('layout.css.prefers-color-scheme.content-override', 0);
